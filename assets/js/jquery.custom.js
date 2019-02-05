60700bc71828c73d1e6613f48092eca91b328e82
DrQuarzZz <tobijafischer@gmail.com>
2019-02-05 18:10:29 +0100

    Some minor changes

--- assets/js/jquery.custom.js
+++ assets/js/jquery.custom.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
 }); //jQuery document ready
 
 $(window).on("load", function(){
-
+    $.get("https://dog.ceo/api/breeds/image/random", function(data) {
+        $('#main-dog').attr("src", data.message);
+    });
 }); //jQuery window loaded
